0ba147bbfd19c1a7c1937e5d02ba37bb0ca100a7
Pavibritta <pavithrarajase1@gmail.com>
2025-06-13 13:37:44 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Navbar from "./Components/Navbar"
-import {BrowserRouter as HashRouter,Routes,Route} from 'react-router-dom'
+import {HashRouter,Routes,Route} from 'react-router-dom'
 import Home from "./Components/Home"
 import About from './Components/About'
 import Contact from "./Components/Contact"
 import Projects from "./Components/Projects"
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Login from "./Components/Login"
+
 function App() {
   return (
     <>
@@ -15,7 +15,7 @@ function App() {
         <Route path='/' element={<Home />} />
 <Route path='/about' element={<About />} />
 <Route path='/contact' element={<Contact />} />
-<Route path='/Projects' element={<Projects />} />
+<Route path='/projects' element={<Projects />} />
       </Routes>
     </HashRouter>
       
