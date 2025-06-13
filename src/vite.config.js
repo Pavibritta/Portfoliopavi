b5315f42e1cb473da126420548769d3860417c57
Pavibritta <pavithrarajase1@gmail.com>
2025-06-13 13:29:22 +0530

    modified

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/PavithraPortfolio/", // Set this to your GitHub repo name
+  base: "/Portfoliopavi/", // Set this to your GitHub repo name
   plugins: [react()],
 });
